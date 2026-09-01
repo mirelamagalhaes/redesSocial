3b875f643ac3a1bd217d8286038c7667d88c9674
mirelamagalhaes <mirelamagalhaes14@gmail.com>
2026-09-01 09:02:33 -0300

    ok

---
+++ style.css/script1.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const likeBtn = documentq.uerySelector(".like-btn");
+    const postMedia = document.querySelector("post-media");
+if(!likeBtn) return;
+
+const likesCountspan = likeBtn.querySelector(".likes-count");
+const bookmarkBtn = document.querySelector("boolmark-btn");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
