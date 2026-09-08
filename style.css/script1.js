d38c6617b6b056db18be914ece604a7f7a256bb3
mirelamagalhaes <mirelamagalhaes14@gmail.com>
2026-09-08 08:57:58 -0300

    ok

--- style.css/script1.js
+++ style.css/script1.js
@@ -6,6 +6,33 @@ if(!likeBtn) return;
 const likesCountspan = likeBtn.querySelector(".likes-count");
 const bookmarkBtn = document.querySelector("boolmark-btn");
 
+let isLiked = false;
+let baseLikes = 0;
+
+if (likesCountspan){
+    likesCountspan.textContente = "0";
+}
+
+//números grandes
+
+function formatLikes(num) {
+    if(num >=1000){
+        return (num/1000).toFixad(1)+"K";
+        }
+        return num.toString();
+}
+
+function addLike(){
+    baseLikes++;
+    isLiked  = true;
+    likeBtn.classList.add("liked");
+    
+    if(likesCountspan){
+        likesCountspan.textContent = formatLikes(baseLikes);
+    }
+        }
+    }
+
 
 
 
